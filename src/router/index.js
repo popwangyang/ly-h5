1768d9c452ceda730df22ca9a1e25e368ca0ae8e
wangyan <1375441740@qq.com>
2020-05-06 21:03:21 +0800

    保存

--- src/router/index.js
+++ src/router/index.js
@@ -34,27 +34,25 @@ if (process.env.NODE_ENV === 'production') {
 const router = new Router(result)
 
 const LOGIN_PAGE_NAME = 'login'
+const HOME_PAGE_NAME = 'mainPage'
 
 router.beforeEach((to, from, next) => {
 	const token = getToken();
-	const theme = store.state.app.theme;
 	if (!token && to.name != LOGIN_PAGE_NAME ) { // 未登录且要跳转的页面不是登录页
+	    console.log(1);
 		next({
 			name: LOGIN_PAGE_NAME,
 		})
 	} else if (!token && to.name == LOGIN_PAGE_NAME) { // 未登录想要跳到登陆页面;
+	    console.log(2);
 		next();
 	} else if (token && to.name == LOGIN_PAGE_NAME) { // 已登录想要跳到登陆页面;
-	    let path = null;
-		if(store.state.app.theme && store.state.app.theme.tabBottoms[0].path){
-			path = store.state.app.theme.tabBottoms[0].path;
-		}else{
-			path = LOGIN_PAGE_NAME;
-		}
-		next({
-			path: path
-		})
+	   console.log(3);
+	    next({
+	    	name: HOME_PAGE_NAME
+	    })
 	} else {
+		console.log(4);
 		next();
 	}
 })
