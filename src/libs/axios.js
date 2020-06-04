b98962c8c2477f706716a4b9110eeb60d60e8629
songbaobao <songbaobao>
2020-06-04 17:07:04 +0800

    保存

--- src/libs/axios.js
+++ src/libs/axios.js
@@ -1,84 +1,82 @@
 import axios from 'axios'
 import store from '@/store'
 import {
-	getToken,
-	setToken,
-	throwError
+  getToken,
+  setToken,
+  throwError
 } from './util'
 import {
-	Toast
+  Toast
 } from 'vant'
 import router from '../router'
 
 class HttpRequest {
-	constructor(baseUrl = baseURL) {
-		this.baseUrl = baseUrl
-	}
-	getInsideConfig() {
-		const config = {
-			baseURL: this.baseUrl,
+  constructor(baseUrl = baseURL) {
+    this.baseUrl = baseUrl
+  }
+  getInsideConfig() {
+    const config = {
+      baseURL: this.baseUrl,
 
-			headers: {
-				'Content-Type': 'application/json;'
-			}
+      headers: {
+        'Content-Type': 'application/json;'
+      }
 
-		}
-		return config
-	}
+    }
+    return config
+  }
 
-	interceptors(instance, skipUpdateToken) {
-		// 请求拦截
-		
-		instance.interceptors.request.use(config => {
-			// 动态添加token
-			console.log(skipUpdateToken);
-			let token = getToken(skipUpdateToken)
-			if (token) {
-				config.headers.Authorization = token;
-			}
+  interceptors(instance, skipUpdateToken, skipThrowError = false) {
+    // 请求拦截
 
-			return config
-		}, error => {
-			return Promise.reject(error)
-		})
-		// 响应拦截
+    instance.interceptors.request.use(config => {
+      // 动态添加token
+      let token = getToken(skipUpdateToken)
+      if (token) {
+        config.headers.Authorization = token;
+      }
 
-		instance.interceptors.response.use(response => {
+      return config
+    }, error => {
+      return Promise.reject(error)
+    })
+    // 响应拦截
 
-			const {
-				data,
-				status
-			} = response
-			return {
-				data,
-				status
-			}
-		}, error => {
-			console.log(error)
-			const {
-				data,
-				status
-			} = error.response
-			if (status === 401 || status === 403) {
-				setToken('')
-				router.push({
-					name: 'login'
-				})
-			}
-			if (status == 400) {
-				throwError(data)
-			}
-			return Promise.reject({
-				data,
-				status
-			})
-		})
-	}
-	request(options) {
-		const instance = axios.create()
-		options = Object.assign(this.getInsideConfig(), options)
-		this.interceptors(instance, options.skipUpdateToken)
-		return instance(options)
-	}
+    instance.interceptors.response.use(response => {
+
+      const {
+        data,
+        status
+      } = response
+      return {
+        data,
+        status
+      }
+    }, error => {
+      const {
+        data,
+        status
+      } = error.response
+      if (status === 401 || status === 403) {
+        setToken('')
+        router.push({
+          name: 'login'
+        })
+      }
+      if (status == 400 && !skipThrowError) {
+        throwError(data)
+      }
+      return Promise.reject({
+        data,
+        status
+      })
+    })
+  }
+  request(options) {
+    const instance = axios.create()
+    options = Object.assign(this.getInsideConfig(), options)
+    this.interceptors(instance, options.skipUpdateToken, options.skipThrowError)
+    return instance(options)
+  }
 }
 export default HttpRequest
