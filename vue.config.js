776176d911a689dfc0f6d442ad455ec5f66cecf2
bao <bao@bao.baoshuai>
2020-07-08 11:16:37 +0800

    打包体积配置优化

--- vue.config.js
+++ vue.config.js
@@ -1,42 +1,56 @@
-const GenerateAssetPlugin = require('generate-asset-webpack-plugin');
-const config = require('./src/config/config.js');
-let createConfig = function (compilation) {
+const GenerateAssetPlugin = require("generate-asset-webpack-plugin");
+const CompressionPlugin = require("compression-webpack-plugin");
+const config = require("./src/config/config.js");
+let createConfig = function(compilation) {
   return JSON.stringify(config);
-}
+};
 
 module.exports = {
-  publicPath: './',
+  publicPath: "./",
   // 如果你不需要使用eslint，把lintOnSave设为false即可
   lintOnSave: false,
+  productionSourceMap: false,
   css: {
     loaderOptions: {
       postcss: {
         plugins: [
-          require('postcss-pxtorem')({
+          require("postcss-pxtorem")({
             rootValue: 14, // 换算的基数
-            selectorBlackList: ['weui', 'mu'], // 忽略转换正则匹配项
-            propList: ['*'],
-          }),
+            selectorBlackList: ["weui", "mu"], // 忽略转换正则匹配项
+            propList: ["*"]
+          })
         ]
       }
     }
   },
   chainWebpack: config => {
     config.module
-      .rule('pdf')
+      .rule("pdf")
       .test(/\.pdf$/)
-      .use('file-loader')
-      .loader('file-loader')
-      .end()
+      .use("file-loader")
+      .loader("file-loader")
+      .end();
   },
-  configureWebpack: {
-    plugins: [
+  configureWebpack: config => {
+    let plugins = [
       new GenerateAssetPlugin({
-        filename: 'config/config.json',
+        filename: "config/config.json",
         fn: (compilation, cb) => {
-          cb(null, createConfig(compilation))
+          cb(null, createConfig(compilation));
         }
       })
-    ]
+    ];
+    if (process.env.NODE_ENV === "production") {
+      plugins.push(
+        new CompressionPlugin({
+          test: /\.js$|\.html$|\.css/,
+          threshold: 10240,
+          deleteOriginalAssets: true
+        })
+      );
+    }
+    return {
+      plugins
+    };
   }
-}
+};
